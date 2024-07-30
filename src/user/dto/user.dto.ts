16963a9042ba1a204e733439cf700157316999f7
Junior M. Soares <q56384@gmail.com>
2024-07-30 16:43:47 +0000

    remove decorators do class-validator

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,22 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsTaxId, IsNotEmpty, IsPostalCode } from 'class-validator';
 
 export class CreateUserDTO {
   @ApiProperty()
-  @IsNotEmpty()
   name: string;
 
   @ApiProperty()
-  @IsNotEmpty()
-  @IsEmail()
   email: string;
 
   @ApiProperty()
-  @IsNotEmpty()
-  @IsTaxId('pt-BR')
   cpf: string;
 
   @ApiProperty({ required: false })
-  @IsPostalCode('BR')
   cep?: string;
 }
